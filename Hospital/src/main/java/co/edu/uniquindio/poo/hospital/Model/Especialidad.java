db05deca4e01ae1a5978d4ca1172c022376f1019
Santiago-Bayona <Cesars.bayonap@uqvirtual.edu.co>
2025-03-12 21:50:14 -0500

    Delete Hospital directory

--- Hospital/src/main/java/co/edu/uniquindio/poo/hospital/Model/Especialidad.java
+++
@@ -1,8 +0,0 @@
-package co.edu.uniquindio.poo.hospital.Model;
-
-public enum Especialidad {
-    PEDIATRIA, CARDIOLOGIA,
-    NEUMOLOGIA, GERIATRIA,
-    MEDICOGENERAL, GINECOLOGIA,
-    PSIQUIATRIA
-}
